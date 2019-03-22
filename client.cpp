b965c2193f7ec5dfee7798f970c56b27bbe0275f
p0l0satik <denis.cucu@yandex.ru>
2019-03-22 22:46:48 +0300

    simple client server test

---
+++ client.cpp
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+char message[] = "Hello there!\n";
+char buf[sizeof(message)];
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(18667);
+    struct in_addr *inp = &addr.sin_addr;
+    inet_aton("127.0.0.1", inp);;
+
+    if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect");
+        exit(2);
+    }
+
+    send(sockfd, message, sizeof(message), 0);
+    int pid = getpid();
+    send(sockfd, &pid, sizeof(int), 0);
+    recv(sockfd, buf, sizeof(message), 0);
+    
+    puts(buf);
+    close(sockfd);
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,61 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    int sockfd, listener;
+    struct sockaddr_in addr;
+    char buf[1024];
+    int bytes_read;
+
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    if(listener < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+    
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(18667);
+    struct in_addr *inp = &addr.sin_addr;
+    inet_aton("127.0.0.1", inp);
+
+    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        exit(2);
+    }
+
+    listen(listener, 4);
+    
+    while(1)
+    {
+        sockfd = accept(listener, NULL, NULL);
+        if(sockfd < 0)
+        {
+            perror("accept");
+            exit(3);
+        }
+
+        while(1)
+        {
+            bytes_read = recv(sockfd, buf, 1024, 0);
+            if(bytes_read <= 0) break;
+            send(sockfd, buf, bytes_read, 0);
+            int pid = 0;
+            bytes_read = recv(sockfd, &pid, sizeof(pid), 0);
+            printf("%d\n", pid);
+            
+        }
+    
+        close(sockfd);
+    }
+    
+    return 0;
+}
