80e6d916efcff511a04f4b318af9f2c04bf7f644
p0l0satik <denis.cucu@yandex.ru>
2019-03-25 20:33:57 +0300

    added aend/recv func

--- client.cpp
+++ client.cpp
@@ -6,14 +6,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <iostream>
 
 char message[] = "Hello there!\n";
 char buf[sizeof(message)];
 
-int main()
-{
-    int sockfd;
-    struct sockaddr_in addr;
+
+void con_to_sv(int &sockfd, const int port, const char* serv_ip) {
+    struct sockaddr_in addr; //socket address
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0)
@@ -23,22 +23,58 @@ int main()
     }
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(14888);
+    addr.sin_port = htons(port);
     struct in_addr *inp = &addr.sin_addr;
-    inet_aton("172.17.0.6", inp);;
+    inet_aton(serv_ip, inp);
 
     if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("connect");
         exit(2);
     }
+}
 
-    send(sockfd, message, sizeof(message), 0);
-    int pid = getpid();
-    send(sockfd, &pid, sizeof(int), 0);
-    recv(sockfd, buf, sizeof(message), 0);
+int send_arr(int sockfd, int *arr, int len, int flags) {
+    int alr_sent = 0;
+    int n = 0;
+    while(alr_sent < len / sizeof(int)) {
+        
+        n = send(sockfd, (arr + alr_sent), len - alr_sent * sizeof(int), flags);
+        if (n == -1) {
+            std::cout << "error while sending" << std::endl;
+            return -1;
+        }
+        alr_sent += n / sizeof(int);
+    }
+    return alr_sent;
+}
+
+int recv_arr(int sockfd, int *arr, int len, int flags) {
+    int alr_recv = 0;
+    int n = 0;
+    while(alr_recv < len / sizeof(int)) {
+        
+        n = recv(sockfd, (arr + alr_recv), len - alr_recv * sizeof(int), flags);
+        if (n == -1) {
+            std::cout << "error while receiving" << std::endl;
+            return -1;
+        }
+        alr_recv += n / sizeof(int);
+    }
+    return alr_recv;
+}
+
+int main()
+{
+    int sockfd; //client fd
+    const char serv_ip[] = "127.0.0.1";
+    const int port = 14888;
+    con_to_sv(sockfd, port, serv_ip);
+    std::cout << "s" << std::endl;
+    int arr1[] = {1, 2, 3, 4, 5, 6, 7};
+    send_arr(sockfd, arr1, sizeof(int) * 7, 0);
     
-    puts(buf);
+    // puts(buf);
     close(sockfd);
 
     return 0;
--- server.cpp
+++ server.cpp
@@ -6,14 +6,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <iostream>
 
-int main()
-{
-    int sockfd, listener;
+int send_arr(int sockfd, int *arr, int len, int flags) {
+    int alr_sent = 0;
+    int n = 0;
+    while(alr_sent < len / sizeof(int)) {
+        
+        n = send(sockfd, (arr + alr_sent), len - alr_sent * sizeof(int), flags);
+        if (n == -1) {
+            std::cout << "error while sending" << std::endl;
+            return -1;
+        }
+        alr_sent += n / sizeof(int);
+    }
+    return alr_sent;
+}
+
+int recv_arr(int sockfd, int *arr, int len, int flags) {
+    int alr_recv = 0;
+    int n = 0;
+    while(alr_recv < len / sizeof(int)) {
+        
+        n = recv(sockfd, (arr + alr_recv), len - alr_recv * sizeof(int), flags);
+        if (n == -1) {
+            std::cout << "error while receiving" << std::endl;
+            return -1;
+        }
+        alr_recv += n / sizeof(int);
+    }
+    return alr_recv;
+}
+
+void start_sv(int &listener, const int port, const int q_size) {
     struct sockaddr_in addr;
-    char buf[1024];
-    int bytes_read;
-
     listener = socket(AF_INET, SOCK_STREAM, 0);
     if(listener < 0)
     {
@@ -22,7 +48,7 @@ int main()
     }
     
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(14888);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
@@ -31,29 +57,40 @@ int main()
         exit(2);
     }
 
-    listen(listener, 4);
+    if (listen(listener, q_size) == -1){
+        perror("listen");
+        exit(3);
+    }
+} 
+
+void ac_cl(int &sockfd, const int listener) {
+    sockfd = accept(listener, NULL, NULL);
+    if(sockfd < 0)
+    {
+        perror("accept");
+        exit(3);
+    }
+}
+
+int main()
+{
+    int client, listener; // client and server fds
+    const int port = 14888;
+    const int q_size = 4;
+    start_sv(listener, port, q_size);
+
     
     while(1)
     {
-        sockfd = accept(listener, NULL, NULL);
-        if(sockfd < 0)
-        {
-            perror("accept");
-            exit(3);
-        }
-
-        while(1)
-        {
-            bytes_read = recv(sockfd, buf, 1024, 0);
-            if(bytes_read <= 0) break;
-            send(sockfd, buf, bytes_read, 0);
-            int pid = 0;
-            bytes_read = recv(sockfd, &pid, sizeof(pid), 0);
-            printf("%d\n", pid);
-            
+        ac_cl(client, listener);   
+        int arr[10];
+        int bytes = recv_arr(client, arr, sizeof(int) * 7, 0);
+        std::cout << "read:" << bytes << std::endl;
+        for(int t = 0; t < 7; t++) {
+            std::cout << arr[t] << std::endl;
         }
     
-        close(sockfd);
+        close(client);
     }
     
     return 0;
