74e28ec510b969812e24412f16ba620a07182130
p0l0satik <denis.cucu@yandex.ru>
2019-03-27 19:46:13 +0300

    added entry from file

--- client.cpp
+++ client.cpp
@@ -7,10 +7,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <iostream>
+#include <fstream>
 
-
-const int BUF_SIZE = 3*2;
-
+const int BUF_SIZE = 3*400;
+const int INF = 1000000000;
 
 void con_to_sv(int &sockfd, const int port, const char* serv_ip) {
     struct sockaddr_in addr; //socket address
@@ -87,22 +87,44 @@ int recv_int(int sockfd, int &snd, int flags) {
     return bytes;
 }
 
-void send_data_about_graph(int sockfd) {
+void send_data_about_graph(int sockfd, int mode) {
     int a, b, c;
     int m, n;
     int buf[BUF_SIZE];
+    std::ifstream inp;
+
+    if (mode == 2) {
+        const int MAX_PATH = 4096;
+        char filepath[MAX_PATH];
+        std::cin >> filepath;
+        inp.open(filepath); 
+        while (!inp.is_open()) {
+            std::cout << "Unable to open your file, please try again" << std::endl;
+            std::cin >> filepath;
+            inp.open(filepath);
+        } 
+    }
 
     send_int(sockfd, BUF_SIZE, 0);
 
-    std::cout << "enter number of nodes:" << std::endl;
-    std::cin >> n;
+    if (mode == 1) {
+        std::cout << "enter number of nodes:" << std::endl;
+        std::cin >> n;
+    } else {
+        inp >> n;
+    }
+    
     send_int(sockfd, n, 0);
 
     std::cout << "enter the nodes:" << std::endl;
     for(int t = 0, j = 0; t <= n; ++t, ++j) {
         // std::cout <<"j is " << j << std:: endl;
         if (t < n) {
-            std::cin >> a;
+            if (mode == 1) {
+                std::cin >> a;
+            } else {
+                inp >> a;
+            }
             buf[j] = a;
             if (j + 1 == BUF_SIZE) { //send when buf is full
                 send_arr(sockfd, buf, BUF_SIZE * sizeof(int), 0);
@@ -115,14 +137,25 @@ void send_data_about_graph(int sockfd) {
         }
     }
 
-    std::cout << "enter number of edges:" << std::endl;
-    std::cin >> m;
+    if (mode == 1) {
+        std::cout << "enter number of edges:" << std::endl;
+        std::cin >> m;   
+    } else {
+        inp >> m;
+    }
+    
     send_int(sockfd, m, 0);
-
-    std::cout << "enter the edges from, to and cost:" << std::endl;
+    if (mode == 1){
+        std::cout << "enter the edges in format - node_from, node_to and edge_cost:" << std::endl;
+    }
     for(int t = 0, j = 0; t <= m; t++) {
         if (t < m) {
-            std::cin >> a >> b >> c;
+            if (mode == 1) {
+                std::cin >> a >> b >> c;
+            } else {
+                inp >> a >> b >> c;
+            }
+            
             buf[j++] = a;
             buf[j++] = b;
             buf[j++] = c;
@@ -131,6 +164,7 @@ void send_data_about_graph(int sockfd) {
                 j = 0;
             }
         } else {
+            std::cout << "j: " << j << std::endl;
             if (j != 0) { //send what is left(if there is smth)
                 send_arr(sockfd, buf, j * sizeof(int), 0);
             }
@@ -139,10 +173,32 @@ void send_data_about_graph(int sockfd) {
     }
 }
 
+void send_from_file(int sockfd) {
+    std::cout << "Data in your file should be specified in a special order:" << std::endl;
+    std::cout << "Number of nodes" << std::endl; 
+    std::cout << "The list of nodes (a number for each node)" << std::endl;
+    std::cout << "number of edges" << std::endl;
+    std::cout << "The list of edges (three numbers for each edge:node_from, node_to and edge_cost)" << std::endl;
+    std::cout << "Enter path to you file:" << std::endl;
+    send_data_about_graph(sockfd, 2);
+}
 void talk_with_serv(int sockfd) {
     int pid = getpid();
     send_int(sockfd, pid, 0);
-    send_data_about_graph(sockfd);
+
+    int how;
+    std::cout << "How would you like to enter the graph? Enter the number:" << std::endl;
+    std::cout << "1 - via a command line" << std::endl;
+    std::cout << "2 - via a file" << std::endl;
+    std::cin >> how;
+    if (how == 1) {
+        send_data_about_graph(sockfd, 1);
+    } else if (how == 2) {
+        send_from_file(sockfd);
+    } else {
+        std::cout << "wrong mode" << std::endl;
+    }
+    
     
     while (1) {
         std::cout << "Do you want to enter request? Enter 1 for 'yes', 0 for 'no':" << std::endl;
@@ -159,6 +215,9 @@ void talk_with_serv(int sockfd) {
         send_arr(sockfd, args, sizeof(int) * 2, 0);
         int len;
         recv_int(sockfd, len, 0);
+        if (len == INF) {
+            
+        }
         std::cout << "Result:" << len << std::endl;
 
     }
